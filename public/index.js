55dbbe2101c50fb9811d66d2594e847325b1f042
ranwahle <ran.wahle@gmail.com>
2018-01-07 08:44:40 +0200

    Movies result from server

--- public/index.js
+++ public/index.js
@@ -3,8 +3,8 @@ import fetchMovies from './fetchMovies.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    document.getElementById('btnSearch').addEventListener('click', () => {
-        fetchMovies(document.getElementById('txtSearchForMovie').value).then(result => {
+    document.getElementById('btnSearch').addEventListener('click',async () => {
+       const result = await fetchMovies(document.getElementById('txtSearchForMovie').value)
             console.log('fetch result', result)
             result.forEach(item => {
                const listItem = document.createElement('li');
@@ -12,7 +12,7 @@ document.addEventListener('DOMContentLoaded', () => {
                document.getElementById('lstResults').appendChild(listItem);
             });
 
-        });
+
     })
 
 

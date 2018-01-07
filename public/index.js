906d65164a0504e4b5390fc6d4c9779cc581e9fb
ranwahle <ran.wahle@gmail.com>
2018-01-07 08:45:12 +0200

    indentation

--- public/index.js
+++ public/index.js
@@ -3,18 +3,18 @@ import fetchMovies from './fetchMovies.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 
-    document.getElementById('btnSearch').addEventListener('click',async () => {
-       const result = await fetchMovies(document.getElementById('txtSearchForMovie').value)
-            console.log('fetch result', result)
-            result.forEach(item => {
-               const listItem = document.createElement('li');
-               listItem.innerText = item.show.name;
-               document.getElementById('lstResults').appendChild(listItem);
-            });
+    document.getElementById('btnSearch').addEventListener('click', async () => {
+        const result = await fetchMovies(document.getElementById('txtSearchForMovie').value)
+
+        console.log('fetch result', result)
+        result.forEach(item => {
+            const listItem = document.createElement('li');
+            listItem.innerText = item.show.name;
+            document.getElementById('lstResults').appendChild(listItem);
+        });
 
 
     })
-
 
 
 })
